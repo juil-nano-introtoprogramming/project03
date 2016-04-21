75c4c7e27960c27f9514ee46c3c36f00fc3285f5
Juil Yoon <jiy.juil@gmail.com>
2016-04-21 10:01:07 +0900

    Add rename_files.py

---
+++ rename_files.py
@@ -0,0 +1,18 @@
+# Lesson 3.2: Use Functions
+# Mini-Project: Secret Message
+
+# Your friend has hidden your keys! To find out where they are,
+# you have to remove all numbers from the files in a folder
+# called prank. But this will be so tedious to do!
+# Get Python to do it for you!
+
+# Use this space to describe your approach to the problem:
+# 1) Find all files in given directory
+# 2) Rename files (remove numbers)
+
+"""Rename file script.
+
+This script takes a given directory and renames every file within to
+not contain numbers.
+
+In special cases, this reveals a secret message in the reordered images."""
