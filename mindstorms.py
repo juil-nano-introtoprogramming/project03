203b265fe5c94d9c73cf0413a0ca53fb6bf12dcd
Juil Yoon <jiy.juil@gmail.com>
2016-04-24 20:28:13 +0900

    Add draw_flower and draw_tri_fractal

--- mindstorms.py
+++ mindstorms.py
@@ -19,70 +19,98 @@ def start():
     window.bgcolor('#AEF')
     return window
 
-def draw_square(length, angle):
+def draw_square(turtle, length):
     """Draw square with given side length."""
-    brad = turtle.Turtle()
-    brad.shape('square')
-    brad.color('#164','#AEB')
-    brad.speed(9)
-
-    brad.right(angle)
-
     for side in xrange(0,4):
-        brad.forward(length)
-        brad.right(90)
+        turtle.forward(length)
+        turtle.right(90)
 
-    return brad
-
-def draw_circle(radius):
-    """Draw circle with given radius."""
-    alex = turtle.Turtle()
-    alex.shape('circle')
-    alex.color('#833','#FBB')
-    alex.speed(8)
-
-    alex.circle(radius)
-
-    return alex
-
-def draw_triangle(length):
+def draw_triangle(turtle, length, fill):
     """Draw triange with given side length."""
-    cris = turtle.Turtle()
-    cris.shape('triangle')
-    cris.color('#146','#3AF')
-    cris.speed(8)
-
-    cris.right(180)
+    turtle.fill(fill)
     for side in xrange(0,3):
-        cris.forward(length)
-        cris.left(120)
+        turtle.forward(length)
+        turtle.left(120)
+    turtle.fill(False)
 
-    return cris
+def draw_rand_squares(turtle):
+    """Draws 4 squares in random sizes."""
+    for square in xrange(0,4):
+        multiple = random.randint(1,5)
+        for side in xrange(0,4):
+            turtle.forward(50*multiple)
+            turtle.right(90)
+        turtle.right(90)
 
-def draw_circle_withsquares(radius):
-    div = 20
+def draw_circle_withsquares(turtle, radius, div):
+    """Draws a circle by drawing rotating squares."""
     for i in xrange(0,div):
-        draw_square(radius, 360/div * i)
+        draw_square(turtle, radius)
+        turtle.right(360/div)
 
-def draw_rand_squares():
+def draw_rhombus(turtle, length, squish):
+    """Draws a diamond squished from a square by squish.
+
+    Args:
+        squish (float): Float between 0-1."""
+    for i in xrange(0,2):
+        turtle.forward(length)
+        turtle.right(90-(90*squish))
+        turtle.forward(length)
+        turtle.right(90+(90*squish))
+
+def draw_flower(turtle, radius, div):
+    """Draws a flower circle using diamonds."""
+    for i in xrange(0,div):
+        draw_rhombus(turtle,radius/2,.3)
+        turtle.right(360/div)
+    turtle.right(90)
+    turtle.forward(radius*2)
+
+def draw_tri_fractal(turtle, length, level):
+    """Draws a triangle fractal with given length and levels.
+
+    Args:
+        level (integer): Number >= 1."""
+    # assert level == 1
+    if level == 1:
+        draw_triangle(turtle, length, True)
+        turtle.left(60)
+        turtle.forward(length)
+        turtle.right(60)
+        draw_triangle(turtle, length, True)
+        turtle.right(60)
+        turtle.forward(length)
+        turtle.left(60)
+        draw_triangle(turtle, length, True)
+    else:
+        draw_tri_fractal(turtle, length/2, level-1)
+        turtle.forward(length/2)
+        turtle.left(120)
+        turtle.forward(length)
+        turtle.right(120)
+        draw_tri_fractal(turtle, length/2, level-1)
+        turtle.forward(length/2)
+        turtle.right(120)
+        turtle.forward(length)
+        turtle.left(120)
+        draw_tri_fractal(turtle, length/2, level-1)
+
+
+if __name__ == '__main__':
+    window = start()
+
     brad = turtle.Turtle()
     brad.shape('turtle')
     brad.color('#164','#AEB')
     brad.speed(8)
 
-    for square in xrange(0,4):
-        multiple = random.randint(1,5)
-        for side in xrange(0,4):
-            brad.forward(50*multiple)
-            brad.right(90)
-        brad.right(90)
-
-if __name__ == '__main__':
-    window = start()
     # draw_square(100, 0)
     # draw_circle(100)
     # draw_triangle(150)
-
-    draw_circle_withsquares(100)
+    # draw_circle_withsquares(brad, 100, 30)
+    # draw_rhombus(brad, 100, .5)
+    # draw_flower(brad, 100, 40)
+    draw_tri_fractal(brad, 100, 3)
 
     window.exitonclick()
