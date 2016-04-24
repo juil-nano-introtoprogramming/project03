7455a6cc5b4cb6ad1601d6f2a28a79021fbc749e
Juil Yoon <jiy.juil@gmail.com>
2016-04-24 16:09:11 +0900

    Add draw square, circle, triangle and circle w/ triangle

--- mindstorms.py
+++ mindstorms.py
@@ -9,17 +9,62 @@
 
 """Simple computer graphics program.
 
-Uses the turtle module to draw shapes.
-
-"""
+Uses the turtle module to draw shapes."""
 
 import turtle
 import random
 
-def draw_square():
+def start():
     window = turtle.Screen()
     window.bgcolor('#AEF')
+    return window
 
+def draw_square(length, angle):
+    """Draw square with given side length."""
+    brad = turtle.Turtle()
+    brad.shape('square')
+    brad.color('#164','#AEB')
+    brad.speed(9)
+
+    brad.right(angle)
+
+    for side in xrange(0,4):
+        brad.forward(length)
+        brad.right(90)
+
+    return brad
+
+def draw_circle(radius):
+    """Draw circle with given radius."""
+    alex = turtle.Turtle()
+    alex.shape('circle')
+    alex.color('#833','#FBB')
+    alex.speed(8)
+
+    alex.circle(radius)
+
+    return alex
+
+def draw_triangle(length):
+    """Draw triange with given side length."""
+    cris = turtle.Turtle()
+    cris.shape('triangle')
+    cris.color('#146','#3AF')
+    cris.speed(8)
+
+    cris.right(180)
+    for side in xrange(0,3):
+        cris.forward(length)
+        cris.left(120)
+
+    return cris
+
+def draw_circle_withsquares(radius):
+    div = 20
+    for i in xrange(0,div):
+        draw_square(radius, 360/div * i)
+
+def draw_rand_squares():
     brad = turtle.Turtle()
     brad.shape('turtle')
     brad.color('#164','#AEB')
@@ -32,7 +77,12 @@ def draw_square():
             brad.right(90)
         brad.right(90)
 
-    window.exitonclick()
-
 if __name__ == '__main__':
-    draw_square()
+    window = start()
+    # draw_square(100, 0)
+    # draw_circle(100)
+    # draw_triangle(150)
+
+    draw_circle_withsquares(100)
+
+    window.exitonclick()
