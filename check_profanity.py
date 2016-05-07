b2ab71b4a4dd04263a932b341d40da6cd348b72f
Juil Yoon <jiy.juil@gmail.com>
2016-05-07 08:40:53 -0400

    Functional check_profanity

--- check_profanity.py
+++ check_profanity.py
@@ -2,13 +2,29 @@
 
 import urllib
 
-def read_text():
-    txt = open("movie_quotes.txt")
+def read_text(file):
+    txt = open(file)
     content = txt.read()
-    print content
     txt.close()
+    return content
 
 def check_profanity(text):
-    print urllib.urlopen("http://www.wdylike.appspot.com/?q=%s" % text)
+    """Checks if text contains profanity.
 
-read_text()
+    Args:
+        text (string): Can be a word or sentence.
+    """
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=%s" % text.lower())
+    output = connection.read()
+    connection.close()
+
+    if 'true' in output:
+        print "Profanity alert!"
+    elif 'false' in output:
+        print "Clear for takeoff."
+    else:
+        print "Inconclusive."
+
+if __name__ == '__main__':
+    check_profanity(read_text("movie_quotes.txt"))
+    check_profanity('shit')
