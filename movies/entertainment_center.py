3d5ec8ccb7a31344b87914d4935ddb93d9da0ca6
Juil Yoon <jiy.juil@gmail.com>
2016-05-09 12:19:31 -0400

    Add movies

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -16,5 +16,24 @@ spirited_away = media.Movie("Spirited Away",
                             "http://ia.media-imdb.com/images/M/MV5BMjYxMDcyMzIzNl5BMl5BanBnXkFtZTYwNDg2MDU3._V1_UY268_CR4,0,182,268_AL_.jpg",
                             "https://www.youtube.com/watch?v=ByXuk9QqQkk")
 
+school_of_rock = media.Movie("School of Rock",
+                            "Rock musician becomes high school music teacher.",
+                            "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock.jpg",
+                            "https://www.yotuube.com/watch?v=3PsUJFEBC74")
+
+midnight_in_paris = media.Movie("Midnight in Paris",
+                                "A screenwriter confronts his relationship by travelling back in time at midnight.",
+                                "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
+
+ratatouille = media.Movie("Ratatouille",
+                            "A rat who can cook makes an alliance with a young kitchen worker.",
+                            "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                            "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+hunger_games = media.Movie("Hunger Games",
+                            "A girl must survive the oppressive regime's death games.",
+                            "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                            "https://www.youtube.com/watch?v=4S9a5V9ODuY")
+
 if __name__ == '__main__':
-    spirited_away.show_trailer()
